57df624c319c875f1f5d3f6c6ece964eab201710
IftekharAlamMithu <iftekharalammithu23@gmail.com>
2024-10-20 15:52:10 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-slate-400 h-screen items-center  flex">
+      <h1>MIThu</h1>
+    </main>
+  );
+}
